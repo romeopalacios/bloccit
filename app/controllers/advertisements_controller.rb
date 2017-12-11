3285cfc2feccbad93464a793f321b0adf97d4900
Romeo Palacios <romeopalacios@ymail.com>
2017-12-11 14:12:44 -0800

    working on solving 5 error messages in rspec

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -12,5 +12,17 @@
   end
 
   def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisment][:title]
+    @advertisment.copy = params[:advertisment][:copy]
+    @advertisment.price = params[:advertisment][:price]
+
+     if @advertisment.save
+       flash[:notice] = "Advertisement was saved."
+       redirect_to @advertisment
+     else
+       flash[:error] = "There was an error saving the advertisment. Please try again."
+       render :new
   end
+end
 end
